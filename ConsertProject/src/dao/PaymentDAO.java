ce58984df5a8b75ac3cb71981c52b5fae32805dd
jhcha0611 <jhcha061110@gmail.com>
2025-07-22 12:28:02 +0900

    Update PaymentDAO.java

--- ConsertProject/src/dao/PaymentDAO.java
+++ ConsertProject/src/dao/PaymentDAO.java
@@ -9,8 +9,8 @@ import dto.PaymentDTO;
 
 public class PaymentDAO {
 	public boolean insertPayment(PaymentDTO dto) {
-		String sql = "INSERT INTO payment (u_no, c_no, h_no, pay_amount, pay_card, pay_name, pay_phone, pay_address) "
-				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "INSERT INTO payment (u_no, c_no, h_no, pay_amount, pay_card, pay_name, pay_phone, pay_address, pay_check) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 		try (Connection conn = DBManager.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			pstmt.setInt(1, dto.getU_no());
@@ -21,6 +21,7 @@ public class PaymentDAO {
 			pstmt.setString(6, dto.getPay_name());
 			pstmt.setString(7, dto.getPay_phone());
 			pstmt.setString(8, dto.getPay_address());
+			pstmt.setInt(9, dto.getPay_check());
 
 			int result = pstmt.executeUpdate();
 			return result > 0;
@@ -32,29 +33,27 @@ public class PaymentDAO {
 
 	// 결제정보 저장
 	public boolean addPayment(PaymentDTO dto) {
-	    String sql = "INSERT INTO payment " +
-	        "(u_no, c_no, h_no, pay_amount, pay_card, pay_date, pay_name, pay_phone, pay_address) " +
-	        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	    try (
-	    		Connection conn = DBManager.getConnection();
-	        PreparedStatement pstmt = conn.prepareStatement(sql);
-	    ) {
-	        pstmt.setInt(1, dto.getU_no());
-	        pstmt.setInt(2, dto.getC_no());
-	        pstmt.setInt(3, dto.getH_no());
-	        pstmt.setInt(4, dto.getPay_amount());
-	        pstmt.setString(5, dto.getPay_card());
-	        pstmt.setTimestamp(6, dto.getPay_date());
-	        pstmt.setString(7, dto.getPay_name());
-	        pstmt.setString(8, dto.getPay_phone());
-	        pstmt.setString(9, dto.getPay_address());
+		String sql = "INSERT INTO payment "
+				+ "(u_no, c_no, h_no, pay_amount, pay_card, pay_date, pay_name, pay_phone, pay_address, pay_check) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try (Connection conn = DBManager.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql);) {
+			pstmt.setInt(1, dto.getU_no());
+			pstmt.setInt(2, dto.getC_no());
+			pstmt.setInt(3, dto.getH_no());
+			pstmt.setInt(4, dto.getPay_amount());
+			pstmt.setString(5, dto.getPay_card());
+			pstmt.setTimestamp(6, dto.getPay_date());
+			pstmt.setString(7, dto.getPay_name());
+			pstmt.setString(8, dto.getPay_phone());
+			pstmt.setString(9, dto.getPay_address());
+			pstmt.setInt(9, dto.getPay_check());
 
-	        int result = pstmt.executeUpdate();
-	        return result > 0;
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return false;
+			int result = pstmt.executeUpdate();
+			return result > 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 }
