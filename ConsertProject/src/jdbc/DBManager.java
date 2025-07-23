3e9a139d93a72356050ad9435f9f27097cca8b0e
jhcha0611 <jhcha061110@gmail.com>
2025-07-23 14:37:08 +0900

    Update DBManager.java

--- ConsertProject/src/jdbc/DBManager.java
+++ ConsertProject/src/jdbc/DBManager.java
@@ -44,11 +44,20 @@ public class DBManager {
 			st.executeUpdate("CREATE TABLE time (" + "t_no INT AUTO_INCREMENT PRIMARY KEY, " + "c_no INT, "
 					+ "t_date DATETIME, " + "FOREIGN KEY (c_no) REFERENCES consert(c_no) ON DELETE CASCADE" + ")");
 
-			st.executeUpdate("CREATE TABLE history (" + "h_no INT AUTO_INCREMENT PRIMARY KEY, " + "u_no INT, "
-					+ "c_no INT, " + "h_zone VARCHAR(50), " + "h_buy INT, "
-					+ "h_date DATETIME DEFAULT CURRENT_TIMESTAMP, " + "h_situ VARCHAR(50), "
-					+ "h_reservation CHAR(8) NOT NULL," + "FOREIGN KEY (u_no) REFERENCES user(u_no) ON DELETE CASCADE, "
+			st.executeUpdate("CREATE TABLE payment (" + "pay_no INT AUTO_INCREMENT PRIMARY KEY, " + "u_no INT, "
+					+ "c_no INT, " + "pay_amount INT, " + "pay_date DATETIME DEFAULT CURRENT_TIMESTAMP, "
+					+ "pay_name VARCHAR(50), " + "pay_phone VARCHAR(20), " + "pay_address TEXT, "
+					+ "pay_check TINYINT(1) DEFAULT 0, "
+					+ "FOREIGN KEY (u_no) REFERENCES user(u_no) ON DELETE CASCADE, "
 					+ "FOREIGN KEY (c_no) REFERENCES consert(c_no) ON DELETE CASCADE" + ")");
+
+			st.executeUpdate("CREATE TABLE history (" + "h_no INT AUTO_INCREMENT PRIMARY KEY, " + "u_no INT, "
+					+ "c_no INT, " + "pay_no INT, " + "h_zone VARCHAR(50), " + "h_buy INT, "
+					+ "h_date DATETIME DEFAULT CURRENT_TIMESTAMP, " + "h_situ VARCHAR(50), "
+					+ "h_reservation CHAR(8) NOT NULL, "
+					+ "FOREIGN KEY (u_no) REFERENCES user(u_no) ON DELETE CASCADE, "
+					+ "FOREIGN KEY (c_no) REFERENCES consert(c_no) ON DELETE CASCADE, "
+					+ "FOREIGN KEY (pay_no) REFERENCES payment(pay_no) ON DELETE SET NULL" + ")");
 
 			st.executeUpdate("CREATE TABLE review (" + "r_no INT AUTO_INCREMENT PRIMARY KEY, " + "u_no INT, "
 					+ "c_no INT, " + "h_no INT, " + "r_con TEXT, " + "r_rating INT, "
@@ -61,14 +70,6 @@ public class DBManager {
 					+ "PRIMARY KEY (u_no, c_no), " + "FOREIGN KEY (u_no) REFERENCES user(u_no) ON DELETE CASCADE, "
 					+ "FOREIGN KEY (c_no) REFERENCES consert(c_no) ON DELETE CASCADE, "
 					+ "FOREIGN KEY (r_no) REFERENCES review(r_no) ON DELETE SET NULL" + ")");
-
-			st.executeUpdate("CREATE TABLE payment (" + "pay_no INT AUTO_INCREMENT PRIMARY KEY, " + "u_no INT, "
-					+ "c_no INT, " + "h_no INT, " + "pay_amount INT, " + "pay_card VARCHAR(50), "
-					+ "pay_date DATETIME DEFAULT CURRENT_TIMESTAMP, " + "pay_name VARCHAR(50), "
-					+ "pay_phone VARCHAR(20), " + "pay_address TEXT, " + "pay_check TINYINT(1) DEFAULT 0, "
-					+ "FOREIGN KEY (u_no) REFERENCES user(u_no) ON DELETE CASCADE, "
-					+ "FOREIGN KEY (c_no) REFERENCES consert(c_no) ON DELETE CASCADE, "
-					+ "FOREIGN KEY (h_no) REFERENCES history(h_no) ON DELETE CASCADE" + ")");
 
 			st.executeUpdate("CREATE TABLE zone (" + "z_no INT AUTO_INCREMENT PRIMARY KEY, " + "z_name VARCHAR(50), "
 					+ "z_price INT" + ")");
